e4b970f1a052b23900f6550b8e8690f30aa5db62
Kevin Trevis <ketrevis@made-f0Br5s7.clusters.42paris.fr>
2024-08-19 13:07:19 +0200

    small fix

--- 06/ex01/srcs/main.cpp
+++ 06/ex01/srcs/main.cpp
@@ -4,8 +4,7 @@
 int	main() {
 	Data	data;
 	data.setName("Kevin");
-	uintptr_t	raw;
-	raw = Serializer::serialize(&data);
+	uintptr_t	raw = Serializer::serialize(&data);
 	Data	*unserialized = Serializer::deserialize(raw);
 	std::cout << unserialized->getName() << std::endl;
 }
