aa3cb86ed89a7cd8042cc2cdadafc2ad7f1eb567
root <root@DESKTOP-UFFG1BQ>
2024-08-29 22:17:58 +0200

    float hardcoded

--- 06/ex00/includes/ScalarConverter.hpp
+++ 06/ex00/includes/ScalarConverter.hpp
@@ -17,9 +17,17 @@ class ScalarConverter {
 	~ScalarConverter();
 	ScalarConverter	&operator=(const ScalarConverter &obj);
 	static void		convert(const std::string &str);
+
 	template<typename Target, typename Original>
 	static bool overflows(Original f) {
-		return (f < -std::numeric_limits<Target>::max() || f > std::numeric_limits<Target>::max());
+		Target max = std::numeric_limits<Target>::max();
+		Target min;
+
+		if (std::numeric_limits<Target>::is_iec559) // if f is a float we do that because min returns the lowest positive number 
+			min = -std::numeric_limits<Target>::max(); // for the float and the double instead of the minimum value
+		else
+			min = std::numeric_limits<Target>::min();
+		return (f < min || f > max);
 	}
 	protected:
 	ScalarConverter();
